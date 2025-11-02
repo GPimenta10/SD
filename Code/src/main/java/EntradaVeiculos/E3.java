be66c0f44efd4a7eb2516b50b081574e3369b0f4
Gabriel Afonso Pimenta <8150306@estg.ipp.pt>
2025-11-02 16:58:58 +0000

    Delete Code/src/main/java/EntradaVeiculos directory

--- Code/src/main/java/EntradaVeiculos/E3.java
+++
@@ -1,9 +0,0 @@
-package EntradaVeiculos;
-
-import java.util.List;
-
-public class E3 extends EntradaBase {
-    public E3(String host) {
-        super("E3", host, 6003, List.of("E3", "Cr3", "S"));
-    }
-}
